60ee5a243a50b3d1a9feb1db840eb37bb2eea2b3
PMHStudio <pmhstudio.pmh@gmail.com>
2020-02-26 23:02:35 +0900

    Maybe This is Official Website

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const { readFileSync } = require('fs')
+const { readFileSync, writeFileSync } = require('fs')
 const cors = require('cors')
 const path = require('path').resolve()
 const chalk = require('chalk')
 const http = require('http')
 const https = require('https')
 const express = require('express')
-const maker = require(path + '/functions/maker')
-const renderer = require(path + '/functions/render')
-const apiRouter = require(path + '/router/apiRouter')
-const siteRouter = require(path + '/router/siteRouter')
+const { renderFile } = require('ejs')
 
 const app = express()
 const ssl = { cert: readFileSync(path + '/cert/trinets-cert.pem'), key: readFileSync(path + '/cert/trinets-key.pem') }
 
-app.use(cors())
-app.use('/src', express.static(path + '/src'))
+let count = Number(readFileSync(path + '/data/data').toString('utf-8'))
 
-apiRouter(app, maker)
-siteRouter(app, renderer, maker)
+app.use(cors())
+app.get('/', (_req, res) => {
+  count++
+  renderFile(path + '/page/index.ejs', { count }, (err, str) => {
+    if (err) console.log(err)
+    res.send(str)
+  })
+  if (count === 16777215) console.log(res.ip)
+})
+
+setInterval(() => {
+  writeFileSync(path + '/data/data', count)
+}, 10000)
 
 http.createServer(app).listen(80, () => { console.log(chalk.green('Non-SSL Server is now on http://localhost:80')) })
 https.createServer(ssl, app).listen(443, () => { console.log(chalk.green('SSL Server is now on https://localhost:433')) })
--- router/siteRouter.js
+++
@@ -1,7 +0,0 @@
-function router (app, render, maker) {
-  app.get('/', (req, res) => render(req, res, maker(), 'index.ejs'))
-  app.get('/make', (req, res) => render(req, res, maker(), 'make.ejs'))
-  app.get('/:id', (req, res) => render(req, res, maker(req.params.id), 'index.ejs'))
-}
-
-module.exports = router
