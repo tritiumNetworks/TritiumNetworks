7e5cc51b9c63c3a6e07bbe8527b7595f6e941105
PMHStudio <pmhstudio.pmh@gmail.com>
2020-02-26 19:33:16 +0900

    Error Handler & Cooldown Added

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -1,6 +1,14 @@
+const cooldowns = []
+
 function func (app, maker) {
   app.get('/api/make', (req, res) => {
+    if (cooldowns.includes(req.ip)) return res.send({ success: false, error: '너무 자주 요청하고 있습니다! 잠시만 기다려 주세요' })
+    cooldowns.push(req.ip)
+    setTimeout(() => {
+      cooldowns[cooldowns.indexOf(req.ip)] = ':)'
+    }, 10000)
     const { tops, bottoms } = req.query
+    if (!tops || !bottoms) res.send({ success: false, error: '적어도 윗내용은 적어야 됩니다' })
     if (tops.length > 7) {
       res.send({ success: true, id: maker.push({ tops, bottoms }) })
     } else res.send({ success: false, error: '윗 내용은 적어도 8글자 이상이여야 합니다' })
