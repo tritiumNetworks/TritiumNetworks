2bbf1765a3cb557d7036f1355ec00a9ed161152a
pmh-only <pmhstudio.pmh@gmail.com>
2020-04-15 19:42:12 +0900

    HostFrame Platform

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
+module.exports = { _root: '/main', _socket: false, _cors: true, _parser: [], ready }
+
 const { readFileSync, writeFileSync } = require('fs')
-const cors = require('cors')
 const path = require('path').resolve()
-const chalk = require('chalk')
-const http = require('http')
-const https = require('https')
-const express = require('express')
 const { renderFile } = require('ejs')
+let count = Number(readFileSync(path + '/router/main/data/data').toString('utf-8'))
 
-const app = express()
-const ssl = { cert: readFileSync(path + '/cert/trinets-cert.pem'), key: readFileSync(path + '/cert/trinets-key.pem') }
-
-let count = Number(readFileSync(path + '/data/data').toString('utf-8'))
-
-app.use(cors())
-app.get('/', (_req, res) => {
-  count++
-  renderFile(path + '/page/index.ejs', { count }, (err, str) => {
-    if (err) console.log(err)
-    res.send(str)
+function ready (app) {
+  console.log('main is loaded')
+  app.get('/', (_req, res) => {
+    count++
+    renderFile(path + '/router/main/page/index.ejs', { count }, (err, str) => {
+      if (err) console.log(err)
+      res.send(str)
+    })
+    if (count === 16777215) console.log(res.ip)
   })
-  if (count === 16777215) console.log(res.ip)
-})
+}
 
 setInterval(() => {
-  writeFileSync(path + '/data/data', count)
+  writeFileSync(path + '/router/main/data/data', count)
 }, 10000)
-
-http.createServer(app).listen(80, () => { console.log(chalk.green('Non-SSL Server is now on http://localhost:80')) })
-https.createServer(ssl, app).listen(443, () => { console.log(chalk.green('SSL Server is now on https://localhost:433')) })
