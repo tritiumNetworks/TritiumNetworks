ade831a89bf46f17ee90a8bcf3588f970844b507
pmh-only <pmhstudio.pmh@gmail.com>
2020-05-12 00:00:22 +0900

    Sitemap Added

--- src/js/main.js
+++ src/js/main.js
@@ -24,18 +24,3 @@ document.getElementsByClassName('logo')[0].addEventListener('click', () => {
     }, 500)
   }
 })
-
-fetch('https://api.github.com/orgs/TritiumNetworks/repos')
-  .then((r) => r.json())
-  .then((res) => {
-    document.getElementsByClassName('sect-projects')[0].innerHTML = ''
-    res.forEach((r) => {
-      document.getElementsByClassName('sect-projects')[0].innerHTML +=
-        document.getElementsByClassName('sect-template')[0].innerHTML
-          .replace('{lang}', r.language || 'none')
-          .replace('{name}', r.name)
-          .replace('{desc}', r.description || '')
-          .replace('{infos}', (r.language ? r.language + ' | ' : '') + 'Last Push: ' + new Date(r.pushed_at).toDateString())
-          .replace('{url}', r.html_url)
-    })
-  })
