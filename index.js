56ebd56db5f0ef3b28f1d11f330dd2e946397a36
pmh-only <pmhstudio.pmh@gmail.com>
2020-06-07 15:43:48 +0900

    SSL Unsupport

--- index.js
+++ index.js
@@ -5,24 +5,15 @@ const { readFileSync } = require('fs')
 
 /**
  * @param {import('../../class/Rapp')} app 
- * @param {{ ws: import('socket.io').Server, wss: import('socket.io').Server }} sockets 
+ * @param {import('socket.io').Server} socket 
  */
-function ready (app, sockets) {
+function ready (app, socket) {
   console.log('main is loaded')
   let subtitle = 'Open-Source Software Dev Group'
   app.get('/', (_, res) => res.send(readFileSync(path + '/router/main/page/index.html', { encoding: 'utf-8' })))
-  sockets.ws.on('connect', (s) => {
-    s.emit('main:subtitle', subtitle)
-    s.on('main:subtitle', (sub) => { subtitle = sub; sockets.ws.emit('main:subtitle', subtitle); sockets.wss.emit('main:subtitle', subtitle) })
-  })
-  sockets.wss.on('connect', (s) => {
-    s.emit('main:subtitle', subtitle)
-    s.on('main:subtitle', (sub) => { subtitle = sub; sockets.ws.emit('main:subtitle', subtitle); sockets.wss.emit('main:subtitle', subtitle) })
-    s.on('main:subtitle', (sub) => { subtitle = sub; sockets.ws.emit('main:subtitle', subtitle); sockets.wss.emit('main:subtitle', subtitle) })
-  })
 
-  sockets.wss.on('connect', (s) => {
+  socket.on('connect', (s) => {
     s.emit('main:subtitle', subtitle)
-    s.on('main:subtitle', (sub) => { subtitle = sub; sockets.ws.emit('main:subtitle', subtitle); sockets.wss.emit('main:subtitle', subtitle) })
+    s.on('main:subtitle', (sub) => { subtitle = sub; socket.emit('main:subtitle', subtitle) })
   })
 }
